fbb16ee128003924f0a7d005c5b44e4d4953cc33
Akshay <akshay.pro0311@gmail.com>
2019-09-22 16:45:51 +0530

    Added reset_button

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.tictactoegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button reset_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reset_button = findViewById(R.id.reset_button);
     }
 }
