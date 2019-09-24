998634acc92e14f5713e0c888169a7382bc925ae
Akshay <akshay.pro0311@gmail.com>
2019-09-24 19:07:27 +0530

    Added core functionality. Playable now.

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -9,11 +9,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationSet;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button reset_button;
+    private LinearLayout reset_winnerMessage_layout;
+    private String winner;
+    private TextView winnerMessage;
     private int current_player=0;
+    private int[] gameState = {2,2,2,2,2,2,2,2,2};
+    private int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +46,41 @@ public class MainActivity extends AppCompatActivity {
         set.play(animator1).before(animator2).with(animator3).before(animator4);
         set.start();
 
-        if(current_player == 0){
-            any_button.setText("X");
-            any_button.setTextColor(Color.WHITE);
-            any_button.setTextSize(40f);
-            current_player = 1;
+        int tag;
+        tag = Integer.parseInt(any_button.getTag().toString());
+
+        if(gameState[tag] == 2) {
+            gameState[tag] = current_player;
+
+            if (current_player == 0) {
+                any_button.setText("X");
+                any_button.setTextColor(Color.WHITE);
+                any_button.setTextSize(40f);
+                current_player = 1;
+            } else if (current_player == 1) {
+                any_button.setText("O");
+                any_button.setTextColor(Color.WHITE);
+                any_button.setTextSize(40f);
+                current_player = 0;
+            }
         }
-        else if(current_player == 1){
-            any_button.setText("O");
-            any_button.setTextColor(Color.WHITE);
-            any_button.setTextSize(40f);
-            current_player = 0;
+        for(int[] i : winningPositions){
+            if(gameState[i[0]] == gameState[i[1]] && gameState[i[1]] == gameState[i[2]] && gameState[i[0]] != 2){
+
+                winner = "PLAYER 1 HAS WON!";
+                if(gameState[i[0]] == 1){
+                    winner = "PLAYER 2 HAS WON!";
+                }
+                winnerMessage = findViewById(R.id.winnerMessage_textView);
+                winnerMessage.setText(winner);
+                reset_winnerMessage_layout = findViewById(R.id.reset_winnerMessage_layout);
+                reset_winnerMessage_layout.animate().alpha(1f).scaleX(1.2f).setDuration(500);
+            }
         }
 
-    }
+
+
+}
     public void reset_game(View view){
 
         reset_button = findViewById(R.id.reset_button);
@@ -65,5 +96,33 @@ public class MainActivity extends AppCompatActivity {
         AnimatorSet set = new AnimatorSet();
         set.play(animator1).before(animator2).with(animator3).before(animator4);
         set.start();
+
+        reset_winnerMessage_layout.setAlpha(0f);
+        current_player = 0;
+
+        for(int i=0;i < gameState.length; i++){
+            gameState[i] = 2;
+        }
+
+        Button reset_button00 = findViewById(R.id.button_00);
+        reset_button00.setText("");
+        Button reset_button01 = findViewById(R.id.button_01);
+        reset_button01.setText("");
+        Button reset_button02 = findViewById(R.id.button_02);
+        reset_button02.setText("");
+        Button reset_button10 = findViewById(R.id.button_10);
+        reset_button10.setText("");
+        Button reset_button11 = findViewById(R.id.button_11);
+        reset_button11.setText("");
+        Button reset_button12 = findViewById(R.id.button_12);
+        reset_button12.setText("");
+        Button reset_button20 = findViewById(R.id.button_20);
+        reset_button20.setText("");
+        Button reset_button21 = findViewById(R.id.button_21);
+        reset_button21.setText("");
+        Button reset_button22 = findViewById(R.id.button_22);
+        reset_button22.setText("");
+
+
     }
 }
