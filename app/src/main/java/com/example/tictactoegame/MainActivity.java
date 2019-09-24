8f1a78f29be7c9681575986d90d3f7cb4c1098e2
Akshay <akshay.pro0311@gmail.com>
2019-09-24 22:31:24 +0530

    Added functionality for draw. Fixed the final winner/draw activity.

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -10,16 +10,18 @@ import android.view.View;
 import android.view.animation.AnimationSet;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button reset_button;
-    private LinearLayout reset_winnerMessage_layout;
+    private RelativeLayout reset_winnerMessage_layout;
     private String winner;
     private TextView winnerMessage;
     private int current_player=0;
+    private int no_winner_counter=2;
     private int[] gameState = {2,2,2,2,2,2,2,2,2};
     private int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         tag = Integer.parseInt(any_button.getTag().toString());
 
         if(gameState[tag] == 2) {
+
             gameState[tag] = current_player;
 
             if (current_player == 0) {
@@ -63,22 +66,38 @@ public class MainActivity extends AppCompatActivity {
                 any_button.setTextSize(40f);
                 current_player = 0;
             }
-        }
-        for(int[] i : winningPositions){
-            if(gameState[i[0]] == gameState[i[1]] && gameState[i[1]] == gameState[i[2]] && gameState[i[0]] != 2){
+            for (int[] i : winningPositions) {
+                if (gameState[i[0]] == gameState[i[1]] && gameState[i[1]] == gameState[i[2]] && gameState[i[0]] != 2) {
 
-                winner = "PLAYER 1 HAS WON!";
-                if(gameState[i[0]] == 1){
-                    winner = "PLAYER 2 HAS WON!";
+                    winner = "PLAYER 1 HAS WON!";
+                    if (gameState[i[0]] == 1) {
+                        winner = "PLAYER 2 HAS WON!";
+                    }
+
+                    winnerMessage = findViewById(R.id.winnerMessage_textView);
+                    winnerMessage.setText(winner);
+                    reset_winnerMessage_layout = findViewById(R.id.reset_winnerMessage_layout);
+                    reset_winnerMessage_layout.animate().alpha(1f).scaleX(1.2f).setDuration(500);
+
                 }
+
+            }
+            for(int i=0;i <9; i++){
+                if(gameState[i] == 2){
+                    no_winner_counter = 1;
+                }
+                else{
+                    no_winner_counter = 0;
+                }
+            }
+            if(no_winner_counter == 0){
                 winnerMessage = findViewById(R.id.winnerMessage_textView);
-                winnerMessage.setText(winner);
+                winnerMessage.setTextSize(20);
+                winnerMessage.setText("IT'S A DRAW");
                 reset_winnerMessage_layout = findViewById(R.id.reset_winnerMessage_layout);
                 reset_winnerMessage_layout.animate().alpha(1f).scaleX(1.2f).setDuration(500);
             }
         }
-
-
 
 }
     public void reset_game(View view){
@@ -122,7 +141,6 @@ public class MainActivity extends AppCompatActivity {
         reset_button21.setText("");
         Button reset_button22 = findViewById(R.id.button_22);
         reset_button22.setText("");
-
 
     }
 }
